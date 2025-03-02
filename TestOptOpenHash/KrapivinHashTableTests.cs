e87c31e6e34ee9e0e9712ccdf0b15ff06bee2629
Hafthor <git@hafthor.net>
2025-03-02 15:05:18 -0500

    UT for remove - note failure

--- TestOptOpenHash/KrapivinHashTableTests.cs
+++ TestOptOpenHash/KrapivinHashTableTests.cs
@@ -21,5 +21,20 @@ public class KrapivinHashTableTests {
         
         Assert.AreEqual(false, table.AddOrUpdate("key99", "new value99"));
         Assert.AreEqual("new value99", table.GetValueOrDefault("key99"));
+
+        for (int i = 0; i < nInsert; i += 3) {
+            // TODO: Currently fails at removing key360
+            Assert.IsTrue(table.Remove($"key{i}"), $"Failed removing key{i}");
+        }
+
+        for (int i = 0; i < nInsert; i++) {
+            string? value = table.GetValueOrDefault($"key{i}");
+            if (i % 3 == 0) {
+                Assert.IsNull(value, $"Value for key{i} should be null");
+            } else {
+                Assert.IsNotNull(value, $"Value for key{i} should not be null");
+                Assert.AreEqual($"value{i}", value!);
+            }
+        }
     }
 }
